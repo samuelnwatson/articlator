9ec5b199025328e723ec09f0781f1e0ea694d42e
sam <1002064@gmx.com>
2017-08-17 16:35:55 +0100

    added migrations for articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816162921) do
+ActiveRecord::Schema.define(version: 20170817153221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 20170816162921) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "content", default: "", null: false
+    t.string "title", default: "", null: false
+    t.string "language", default: "", null: false
   end
 
   create_table "users", force: :cascade do |t|
