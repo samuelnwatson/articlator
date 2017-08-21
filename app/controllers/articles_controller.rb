097c965833720aeb25369ebaf7c342f17d412623
sam <1002064@gmx.com>
2017-08-21 11:31:56 +0100

    updated model and controller for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -16,8 +17,10 @@ class ArticlesController < ApplicationController
 
     if @article.save
       redirect_to @article
+      flash[:notice] = 'article created'
     else
       render 'new'
+      flash[:notice] = 'article not created'
     end
   end
 
@@ -25,14 +28,28 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    if @article.update(article_params)
+      redirect_to @article
+      flash[:notice] = 'article updated'
+    else
+      render 'edit'
+      flash[:notice] = 'article not updated'
+    end
   end
 
   def destroy
+    @article.destroy
+    flash[:notice] = 'article destroyed'
+    redirect_to root_path
   end
 
   private
 
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
