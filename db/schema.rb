1ed2cc0dca0374dee9392ae3fd92f4eb948eb3f1
sam <1002064@gmx.com>
2017-08-22 19:30:26 +0100

    added migrations for article and user relation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817153221) do
+ActiveRecord::Schema.define(version: 20170822173320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20170817153221) do
     t.string "content", default: "", null: false
     t.string "title", default: "", null: false
     t.string "language", default: "", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,8 +38,10 @@ ActiveRecord::Schema.define(version: 20170817153221) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
